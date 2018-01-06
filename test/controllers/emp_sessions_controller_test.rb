76f424ee5621753d949b01afa8de2c209b9dac78
Nikhildanand <nikhildanand@gmail.com>
2018-01-06 20:59:44 +0530

    dynamic title content, min and max values for dates

--- test/controllers/emp_sessions_controller_test.rb
+++ test/controllers/emp_sessions_controller_test.rb
@@ -4,17 +4,20 @@ class EmpSessionsControllerTest < ActionDispatch::IntegrationTest
 
   def setup 
     @employee = employees(:employee)
+    @base_title = "Employee Portal"
   end
 
   test "should get login" do
     get employeeportal_login_path
     assert_response :success
+    assert_select "title", "Employee Login | #{@base_title}"
   end
 
   test "should get dashboard" do
     log_in_as @employee
     get employeeportal_dashboard_path
     assert_response :success
+    assert_select "title", "Employee Home | #{@base_title}"
   end
 
 end
--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -5,6 +5,7 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   def setup 
     @employee = employees(:employee)
     @admin = employees(:admin)
+    @base_title = "Employee Portal"
   end
 
   test "should get root" do
@@ -17,41 +18,48 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
     log_in_as @employee
     get employeeportal_path
     assert_response :success
+    assert_select "title", "Edit Details | #{@base_title}"
   end
 
   test "should get admin login" do
     get admin_login_path
     assert_response :success
+    assert_select "title", "Admin Login | #{@base_title}"
   end
 
   test "should get admin dashboard" do
     log_in_as @admin
     get admin_dashboard_path
     assert_response :success
+    assert_select "title", "Admin Dashboard | #{@base_title}"
   end
 
   test "should get employee table" do
     log_in_as @admin
     get admin_employee_path
     assert_response :success
+    assert_select "title", "Employees | #{@base_title}"
   end
 
   test "should get employee details" do
     log_in_as @admin
     get admin_employeedetails_path(:id => @employee.id)
     assert_response :success
+    assert_select "title", "Employee Details | #{@base_title}"
   end
 
   test "should get employee add page" do
     log_in_as @admin
     get admin_addemployee_path
     assert_response :success
+    assert_select "title", "Add Employee | #{@base_title}"
   end
 
   test "should get projects page" do
     log_in_as @admin
     get admin_projects_path
     assert_response :success
+    assert_select "title", "Projects | #{@base_title}"
   end
 
 end
